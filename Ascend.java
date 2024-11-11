fbd52618166d1e45474192fa74d9f952d4dd4de0
Omer Grinvald <omer.grinvald@post.runi.ac.il>
2024-11-11 16:59:08 +0200

    part1

---
+++ Ascend.java
@@ -0,0 +1,15 @@
+// Generates three integer random numbers in a given range,
+// and prints them in increasing order.
+public class Ascend {
+	public static void main(String[] args) {
+		int lim = Integer.parseInt(args[0]);
+		int a = (int) (Math.random() * lim);
+		int b = (int) (Math.random() * lim);
+		int c = (int) (Math.random() * lim);
+		System.out.println(a + " " + b + " " + c);
+		int min = Math.min(a, Math.min(b, c));
+		int max = Math.max(a, Math.min(b, c));
+		int mid = a + b + c - max - min;
+		System.out.println(min + " " + mid + " " + max);
+	}
+}
---
+++ FVCalc.java
@@ -0,0 +1,11 @@
+// Computes the future value of a saving investment.
+public class FVCalc {
+	public static void main(String[] args){
+		int currentValue = Integer.parseInt(args[0]);
+		double rate = Double.parseDouble(args[1])/100;
+		int n = Integer.parseInt(args[2]);
+		double futureValue;
+		futureValue = currentValue * Math.pow((1 + rate), n);
+		System.out.println("after " + n + " years, $" + currentValue + " saved at " + (rate * 100) + "% will yield $" + (int)futureValue);
+	}
+}
